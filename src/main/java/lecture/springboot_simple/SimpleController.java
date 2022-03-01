9fa4dd3f6b3a26e8f94b8210624632dc47bb76d4
Dalton <dalton.bell@astontech.com>
2022-03-01 13:27:42 -0800

    testing ci change to only run builds on src changes

--- src/main/java/lecture/springboot_simple/SimpleController.java
+++ src/main/java/lecture/springboot_simple/SimpleController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Date;
 
+// added comment to test ci
+
 @RestController
 public class SimpleController {
 
--- webapp.tf
+++ webapp.tf
@@ -1,7 +1,7 @@
 module "app" {
     source = "./webapp_deployment"
     application_name = "example-webapp"
-    application_version = "v.01"
+    application_version = "test"
     artifact_bucket = "aston-lecture.springboot.simple-artifacts"
     worker_ami = "ami-0abfe55c2cc86d5ad"
     instance_profile = "allow_s3_ec2_github_demo"
