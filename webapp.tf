08e150147206f0a5a66cf0ba96c47b98fd89864e
Dalton <dalton.bell@astontech.com>
2022-03-01 13:17:40 -0800

    updated launch config for java, linked to AMI

--- webapp.tf
+++ webapp.tf
@@ -3,5 +3,6 @@ module "app" {
     application_name = "example-webapp"
     application_version = "v.01"
     artifact_bucket = "aston-lecture.springboot.simple-artifacts"
-    worker_ami = "ami-0c60d2ab08a174c4e"
+    worker_ami = "ami-0abfe55c2cc86d5ad"
+    instance_profile = "allow_s3_ec2_github_demo"
 }
