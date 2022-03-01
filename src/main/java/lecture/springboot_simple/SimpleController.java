1a789f90ccd371d784af70e8cec2106093304fab
Dalton <dalton.bell@astontech.com>
2022-03-01 13:35:16 -0800

    test ci

--- src/main/java/lecture/springboot_simple/SimpleController.java
+++ src/main/java/lecture/springboot_simple/SimpleController.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Date;
 
-// added comment to test ci
+
+// trigger ci
 
 @RestController
 public class SimpleController {
