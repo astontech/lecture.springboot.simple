413d8ff668c63618a223c407d26108f5a2aa4a60
Dalton <dalton.bell@astontech.com>
2022-03-01 13:40:33 -0800

    update tf app version to latest commit

--- webapp.tf
+++ webapp.tf
@@ -1,7 +1,7 @@
 module "app" {
     source = "./webapp_deployment"
     application_name = "example-webapp"
-    application_version = "test"
+    application_version = "1a789f9"
     artifact_bucket = "aston-lecture.springboot.simple-artifacts"
     worker_ami = "ami-0abfe55c2cc86d5ad"
     instance_profile = "allow_s3_ec2_github_demo"
