8423ab3b2abed2bd8504a3d4f23da587c7861692
Dalton <dalton.bell@astontech.com>
2022-03-02 12:04:27 -0800

    update subnet config

--- webapp.tf
+++ webapp.tf
@@ -5,4 +5,18 @@ module "app" {
     artifact_bucket = "aston-lecture.springboot.simple-artifacts"
     worker_ami = "ami-0abfe55c2cc86d5ad"
     instance_profile = "allow_s3_ec2_github_demo"
+    subnets = [
+    {
+      az     = "us-west-2a",
+      prefix = "10.1.1.0/24"
+    },
+    {
+      az     = "us-west-2b",
+      prefix = "10.1.2.0/24"
+    },
+    {
+      az     = "us-west-2c",
+      prefix = "10.1.3.0/24"
+    }
+  ]
 }
