2d6a172bbd6b13cc9d7611b9bf874e82a25f893c
Bipin <developer.support@astontech.com>
2022-03-02 15:41:05 -0600

    prepare for release. updated app ver in webapp.tf

--- webapp.tf
+++ webapp.tf
@@ -1,7 +1,7 @@
 module "app" {
     source = "./webapp_deployment"
     application_name = "example-webapp"
-    application_version = "88a42a2"
+    application_version = "56de2d8"
     artifact_bucket = "aston-lecture.springboot.simple-artifacts"
     worker_ami = "ami-0abfe55c2cc86d5ad"
     instance_profile = "allow_s3_ec2_github_demo"
