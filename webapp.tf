3e163480ce76166e93ee136048ee66ee13411cbd
Dalton Bell <77699861+daltonb-escalations@users.noreply.github.com>
2022-03-03 15:05:15 -0800

    Update webapp.tf

--- webapp.tf
+++ webapp.tf
@@ -1,7 +1,7 @@
 module "app" {
     source = "./webapp_deployment"
     application_name = "example-webapp"
-    application_version = "9c86927"
+    application_version = "ad03d00"
     artifact_bucket = "aston-lecture.springboot.simple-artifacts-west"
     worker_ami = "ami-0abfe55c2cc86d5ad"
     instance_profile = "allow_s3_ec2_github_demo"
