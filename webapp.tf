ebcf3980b2db342ac1c19a82d397ef31a5b5c1a3
Bipin <developer.support@astontech.com>
2022-03-03 11:44:29 -0600

    pre-release for 9c86927

--- webapp.tf
+++ webapp.tf
@@ -1,7 +1,7 @@
 module "app" {
     source = "./webapp_deployment"
     application_name = "example-webapp"
-    application_version = "55faad8"
+    application_version = "9c86927"
     artifact_bucket = "aston-lecture.springboot.simple-artifacts-west"
     worker_ami = "ami-0abfe55c2cc86d5ad"
     instance_profile = "allow_s3_ec2_github_demo"
