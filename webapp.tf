278dafb595e0c9f253fefa2345e409458e2dc4e8
Bipin <developer.support@astontech.com>
2022-03-09 15:03:52 -0600

    + README with diagrams * Pre Lecture cleanup

--- webapp.tf
+++ webapp.tf
@@ -1,7 +1,7 @@
 module "app" {
     source = "./webapp_deployment"
     application_name = "example-webapp"
-    application_version = "ad03d00"
+    application_version = "a980ba5"
     artifact_bucket = "aston-lecture.springboot.simple-artifacts-west"
     worker_ami = "ami-0abfe55c2cc86d5ad"
     instance_profile = "allow_s3_ec2_github_demo"
